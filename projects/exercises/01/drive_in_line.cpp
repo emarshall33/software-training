c0b9cfccc99b85f2b58e3f0a2a9d17828f9df371
Demo User <debian@beaglebone.localdomain>
2019-10-06 12:04:46 -0400

    goes forward x ft, turns y degrees

--- projects/exercises/01/drive_in_line.cpp
+++ projects/exercises/01/drive_in_line.cpp
@@ -1,12 +1,53 @@
 #include <STSL/RJRobot.h>
 
+void driveForward(double feet, RJRobot& robot);
+void turn(double degrees, RJRobot& robot);
 int main()
 {
     RJRobot robot; //Initialize a RJ Robot
+    driveForward(1, robot);
+    turn(90, robot);
+    //can you see me
+    // Drive forwards
+    // for (int i = 0; i < 4; i++) {
+    //     robot.setDriveMotors(1, -1);
+    //     robot.wait(2000ms);
+    //     robot.setDriveMotors(0, 0);
+    //     robot.wait(800ms);
+    //     robot.setDriveMotors(1, 1);
+    //     robot.wait(135ms);
+    //     robot.setDriveMotors(0, 0);
+    //     robot.wait(800ms);
+    // }
     
-    robot.setDriveMotors(1.0, 1.0); //Drive both robot motors forward at max speed
-    robot.wait(2000ms); //Wait for 2000 milliseconds (2 seconds)
-    robot.stopMotors(); //Stop the motors
+    
+ 
+    // robot.setDriveMotors(1.0, -1.0); //Drive both robot motors forward at max speed
+    // robot.wait(2500ms); //Wait for 2000 milliseconds (2 seconds)
+    // robot.stopMotors(); //Stop the motors
 
+// <<<<<<<<< saved version
+
+// =========
+// =========
+
+// >>>>>>>>> local version
+// >>>>>>>>> local version
     return 0;
 }
+
+
+    void driveForward(double feet, RJRobot& robot)
+    {
+        robot.setDriveMotors(0.25, -0.25);
+        int a = (int)feet;
+        int b = a*2000;
+        robot.wait(std::chrono::milliseconds(b));
+    }
+    void turn(double degrees, RJRobot& robot)
+    {
+        robot.setDriveMotors(-0.25, -0.25);
+        int a = (int)degrees;
+        int b = a*9;
+        robot.wait(std::chrono::milliseconds(b));
+    }
